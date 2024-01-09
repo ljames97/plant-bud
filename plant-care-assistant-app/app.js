577253c439e915bbe9670234df8c23911c458430
Luke <lukedesmondjames@gmail.com>
2024-01-09 14:44:28 +0000

    todo list

---
+++ plant-care-assistant-app/app.js
@@ -0,0 +1,19 @@
+/**
+ * Project: Plant Care Assistant App - Draft Portfolio Project
+ *
+ * Description:
+ * A plant care assistant for discovering suitable plants for the user's home as well as learning about and keeping track of their plants. 
+ *
+ * Version: 1.0.0
+ * Author: Luke James
+ * Created on: 09/01/2024
+ * 
+ */
+
+/**
+ * TODO:
+ * - css and UI design ideas. Find inspiration, sketch out first
+ * - structural draft. Pages, features, misc etc
+ * - core feature js code (plant suitability quiz, schedueler, plant logging, plant information via search, home page)
+ *
+ * */
