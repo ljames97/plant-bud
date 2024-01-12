7cf12529d90fa4fe774519113ce67a34ca545d21
Luke <lukedesmondjames@gmail.com>
2024-01-12 22:43:33 +0000

    css color touches, mobile modal js

--- plant-care-assistant-app/app.js
+++ plant-care-assistant-app/app.js
@@ -1,5 +1,5 @@
 /**
- * Project: Plant Care Assistant App - Draft Portfolio Project (LeafBud)
+ * Project: Plant Care Assistant App - Draft Portfolio Project (PlantBud)
  *
  * Description:
  * A plant care assistant for discovering suitable plants for the user's home as well as learning about and keeping track of their plants. 
@@ -12,7 +12,56 @@
 
 /**
  * TODO:
- * - js for the mobile nav modal
+ * - js for the mobile nav modal, finish basic css for mobile nav
+ * - more buttons or small images on home page
+ * = set up multiple https pages for each feature
  * - core feature simple css and start js code (plant suitability quiz, schedueler, plant logging, plant information via search, dashboard)
+ * - clean up css and public image folder
  *
  * */
+
+const toggleMobileNav = (isOpen) => {
+  const mobileNavModal = document.querySelector('.mobile-nav-modal');
+  if (isOpen) {
+    mobileNavModal.classList.add('show');
+  } else {
+    mobileNavModal.classList.remove('show');
+  }
+}
+
+/**
+ * Function to assign event handlers to static elements
+ */
+const staticEventHandlers = () => {
+  const eventHandlerData = staticEventHandlerManager();
+
+  eventHandlerData.forEach(eventHandler => {
+    const button = document.querySelector(eventHandler.class)
+
+    button.addEventListener('click', () => {
+      eventHandler.handler();
+    });
+  })
+}
+
+/**
+ * Function to manage static elements and their event handlers
+ * @returns event handler data (array)
+ */
+const staticEventHandlerManager = () => {
+  const eventHandlerData = [
+    {
+      class: '.menu-bars',
+      handler: () => toggleMobileNav(true)
+    },
+    {
+      class: '.mobile-nav-close-btn',
+      handler: () => toggleMobileNav(false)
+    }
+  ];
+
+  return eventHandlerData;
+}
+
+
+staticEventHandlers();
