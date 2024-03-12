b756b963281713eea8eda87083309c55653cf994
Luke <lukedesmondjames@gmail.com>
2024-03-12 19:45:10 +0000

    beginning plantQuiz logic

--- plant-care-assistant-app/components/plant-quiz/plantQuiz.js
+++ plant-care-assistant-app/components/plant-quiz/plantQuiz.js
@@ -1,8 +1,13 @@
 //plantQuiz.js
 
+import { questions } from "../data";
 import { createElement, domElements, prepareDashboard } from "../domManipulation"
+import { localEventManager } from "../eventHandling";
 import { appendChildren } from "../utility";
 
+/**
+ * Initialises the plant quiz by prepareing the dashboard and rendering the quiz on screen.
+ */
 export const startPlantQuiz = () => {
   const { plantQuiz, myPlantsBtn, plantQuizBtn, discoverBtn } = domElements;
   prepareDashboard(plantQuizBtn, myPlantsBtn, discoverBtn);
@@ -14,16 +19,25 @@ export const startPlantQuiz = () => {
   }
 }
 
+/**
+ * Renders the initial quiz state on screen.
+ */
 const renderPlantQuiz = () => {
   const { plantQuiz } = domElements;
   const { quizTitle, quizSubheader, startQuizBtn } = createPlantQuizElements();
 
   appendChildren(plantQuiz, quizTitle, quizSubheader, startQuizBtn);
   plantQuiz.classList.add('active');
+  localEventManager.addEventListener(startQuizBtn, 'click', () => {
 
-
+  
+  });
 }
 
+/**
+ * Creates plant quiz elements.
+ * @returns Plant quiz elements.
+ */
 const createPlantQuizElements = () => {
   const quizTitle = createElement({tagName: 'h1', classEl: 'section-title', textContent: 'Plant Quiz'});
   const quizSubheader = createElement({tagName: 'p', classEl: 'quiz-subheader', textContent: 'Take the plant quiz to find out which plants are best suited for you!'});
@@ -31,4 +45,28 @@ const createPlantQuizElements = () => {
   const startQuizBtn = createElement({tagName: 'button', textContent: 'START'});
 
   return { quizTitle, quizSubheader, startQuizBtn };
+}
+
+const startQuizBtnHandler = () => {
+  renderQuestion(questions[0].question, questions[0].answers, );
+}
+
+const renderQuestion = (questionText, choices) => {
+  
+  
+
+}
+
+const userAnswerLog = () => {
+  const userAnswerLog = [];
+
+  return {
+    
+  }
+}
+
+const generatePlantQuizQuestions = () => {
+  
+
+  return questions;
 }
