795784faaeebe5ffec0fcb0682647e8216cc41c4
Luke <lukedesmondjames@gmail.com>
2025-02-03 16:56:45 +0000

    updated build

--- src/index.js
+++ src/index.js
@@ -37,12 +37,4 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("img").forEach(img => {
-    if (!img.hasAttribute("loading")) {
-      img.setAttribute("loading", "lazy");
-    }
-  });
-});
-
 setUpViewHeightListeners();
